4dec2c2cf04772829755760ed07f76398799ee1c
GustavoMnz <59345599+GustavoMnz@users.noreply.github.com>
2022-10-25 18:17:17 -0300

    Update Banco_de_Sangre.sql

--- Banco_de_Sangre.sql
+++ Banco_de_Sangre.sql
@@ -1,30 +1,30 @@
-CREATE DATABASE Banco_de_Sangre;
-GO
-USE Banco_de_Sangre;
-GO
+create database Banco_de_Sangre;
+go
+use Banco_de_Sangre;
+go
 
-CREATE TABLE tipo_sangre
+create table tipo_sangre
 (
-	tipo_sangre_id INT IDENTITY PRIMARY KEY,
-	grupo_sanguineo CHAR(2) not null,
-	factor_rh CHAR(1) not null,
-	CONSTRAINT CK_tipo_sangre_grupo_sanguineo CHECK (grupo_sanguineo in ('A','B','AB','O')),
-	CONSTRAINT CK_tipo_sangre_factor_rh CHECK (factor_rh in ('+','-')),
+	tipo_sangre_id int identity primary key,
+	grupo_sanguineo char(2) not null,
+	factor_rh char(1) not null,
+	constraint CK_tipo_sangre_grupo_sanguineo check (grupo_sanguineo in ('A','B','AB','O')),
+	constraint CK_tipo_sangre_factor_rh check (factor_rh in ('+','-')),
 );
-
-CREATE TABLE sexo
+go
+create table sexo
 (
-	id_sexo INT IDENTITY PRIMARY KEY,
-	descripcion CHAR(1) not null,
-	CONSTRAINT CK__sexo__descripcion CHECK (descripcion in ('F','M')),
+	id_sexo int identity primary key,
+	descripcion char(1) not null,
+	constraint CK__sexo__descripcion check (descripcion in ('F','M')),
 );
-
+go
 create table tipo_hemocomponente
 (
 	id_hemocomponente int identity primary key,
 	descripcion varchar(15),
 );
-
+go
 create table donante 
 (
 	id_donante int primary key,
@@ -40,7 +40,7 @@ create table donante
 	constraint FK__donante__sexo foreign key (id_sexo) references sexo (id_sexo),
 	constraint FK__donante__tipo_sangre foreign key (tipo_sangre_id) references tipo_sangre (tipo_sangre_id)
 );
-
+go
 create table paciente 
 (
 	dni char(8) unique not null,
@@ -55,7 +55,7 @@ create table paciente
 	constraint FK__paciente__sexo foreign key (id_sexo) references sexo (id_sexo),
 	constraint FK__paciente__tipo_sangre foreign key (tipo_sangre_id) references tipo_sangre (tipo_sangre_id)
 );
-
+go
 create table bioanalista 
 (
 	id_bioanalista int primary key,
@@ -69,3 +69,4 @@ create table bioanalista
 	constraint UQ__bioanalista_dni unique (dni),
 	constraint FK__bioanalista__sexo foreign key (id_sexo) references sexo (id_sexo),
 );
+go
