fc7db1f195ca47d6bd89cf7faf4eefcb23c05a2b
GustavoMnz <59345599+GustavoMnz@users.noreply.github.com>
2022-10-25 21:41:19 -0300

    lote de datos para base de datos banco de sangre

---
+++ Lote_de_datos.sql
@@ -0,0 +1,5 @@
+insert into sexo values ('F'),('M');
+
+insert into tipo_sangre values ('A','+'),('A','-'),('B','+'),('B','-'),('AB','+'),('AB','-'),('O','+'),('O','-');
+
+insert into tipo_hemocomponente values ('globulos rojos'),('plaquetas'),('plasma');
